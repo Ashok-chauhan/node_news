41330fede96f2448d8b455d5ddd723e4cf333bfe
Ashok-chauhan <ashok975@gmail.com>
2025-02-09 23:01:51 +0530

    favicon.ico image added

--- src/redisClient.js
+++ src/redisClient.js
@@ -4,16 +4,17 @@ const client = redis.createClient({
   // url: process.env.REDIS_URL,
   // password: process.env.REDIS_PASSWORD,
   // url: "redis://default:aJbA7oDf43UHDIxJYGjLPFO4ZYn2fSyr@redis-19099.c256.us-east-1-2.ec2.redns.redis-cloud.com:19099",
-  /*
+
   socket: {
     host: "13.126.190.13",
     port: 6379,
   },
   password: "ashokkumar",
-*/
 
+  /*
   url: "redis://127.0.0.1:6379",
   password: "ashokkumar",
+  */
 });
 
 client.on("error", (err) => console.error("Redis Client Error", err));
