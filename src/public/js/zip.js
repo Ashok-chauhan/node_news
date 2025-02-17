bfb95c89338cac55d9f22f0cd059fb1226cafa57
Ashok-chauhan <ashok975@gmail.com>
2025-02-17 18:47:58 +0530

    weather added

--- src/public/js/zip.js
+++ src/public/js/zip.js
@@ -8,13 +8,13 @@ navigator.geolocation.getCurrentPosition(
     );
     const data = await response.json();
     zip_code = data.address.postcode;
-    console.log("from nomiatim", zip_code);
+
     // .then((response) => response.json())
     // .then((data) => {
     //   console.log("ZIP Code:", data.address.postcode);
     //   zip_code = data.address.postcode;
     // });
-    zip_code = "03896";
+
     if (zip_code.length === 5) {
       const wResponse = await fetch(
         `https://wx.whizti.com/api/weather/800?zip_code=${zip_code}`
@@ -42,21 +42,11 @@ navigator.geolocation.getCurrentPosition(
     }
   },
   async (error) => {
-    /*
-    if (error.code === error.PERMISSION_DENIED) {
-      console.log("❌ User denied location access.");
-      // getZipfromIP();
-    } else {
-      console.log("⚠️ Error getting location:", error.message);
-      // getZipfromIP();
-    }
-*/
     const responseFromip = await fetch(
       "https://ipinfo.io/json?token=e2131bd04aca82"
     );
     const respData = await responseFromip.json();
     zip_code = respData.postal;
-    console.log("zzzzzzzzzz ", zip_code);
 
     if (zip_code.length === 5) {
       const wResponse = await fetch(
@@ -64,6 +54,24 @@ navigator.geolocation.getCurrentPosition(
       );
       const weatherjson = await wResponse.json();
       console.log("weaather Response", weatherjson);
+      const weather = document.querySelector("#weather");
+      weather.innerHTML = `
+        <div>${weatherjson.CurrentCondition.City}</div>
+        <div>${weatherjson.CurrentCondition.Temperature}&deg;F <span><img src="${weatherjson.CurrentCondition.WeatherIconURL}"/></span></div>
+        `;
+      const weatherHero = document.querySelector("#weatherHero");
+      weatherHero.innerHTML = `<div class="panTemp">${weatherjson.CurrentCondition.Temperature}&deg;F
+       <span><img src="${weatherjson.CurrentCondition.WeatherIconURL}"/></span>
+       <span class="lead">${weatherjson.CurrentCondition.WeatherDescShort}</span>
+       <spna class="display-6">Feels like ${weatherjson.CurrentCondition.RealFeelTemperature}&deg;F</span>
+        </div>
+        
+        `;
+      // const weekly = document.querySelector("#panWeekday");
+      // weatherjson.WeeklyForecast.forEach((element) => {
+      //   weekly.innerHTML += `<div>${element.Date}</div>`;
+      //   weekly.innerHTML += `<div>${element.TempMaxF}</div>`;
+      // });
     }
   }
 );
